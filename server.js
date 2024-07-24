e10510b4a428058850355eb5ff948716fb818d8d
satyam <satyamkale271@gmail.com>
2024-07-24 22:28:07 +0530

    json data passed

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
-app.get("/", (req, res) => {
-  res.send("heeelloooo");
+const friends = [
+  {
+    id: 0,
+    name: "Albert einstein",
+  },
+  {
+    id: 1,
+    name: "Satyam kale",
+  },
+];
+app.get("/friends", (req, res) => {
+  res.json(friends);
 });
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}...`);
