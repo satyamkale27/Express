4780c278e0d7506e8a90c052a82d9ae46ce058c4
satyam <satyamkale271@gmail.com>
2024-07-26 15:01:53 +0530

    400 bad request if no name passed

--- server.js
+++ server.js
@@ -18,7 +18,12 @@ app.use((req, res, next) => {
   console.log(`${req.method} ${req.url} ${delta}ms`);
 });
 app.use(express.json()); // buit in middlleware function to handel json data from request //
-app.post("/friends", (req, req) => {
+app.post("/friends", (req, res) => {
+  if (!req.body.name) {
+    res.status(400).json({
+      error: "Missing friend name",
+    });
+  }
   const newFriend = {
     name: req.body.name,
     id: friends.length,
