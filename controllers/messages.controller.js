8e31ef3b9dec65ad7f548c0102f2eec9d9029e6f
satyam <satyamkale271@gmail.com>
2024-07-29 16:49:00 +0530

    image sent by res.send function

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,5 +1,7 @@
+const path = require("path");
 function getMessages(req, res) {
-  res.send("<ul><li>hello satyam </li></ul>");
+  res.sendFile(path.join(__dirname, "..", "public", "skimountain.jpg"));
+  // res.send("<ul><li>hello satyam </li></ul>");
 }
 function postMessage(req, res) {
   console.log("updating messages...");
