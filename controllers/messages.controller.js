f7b0b19f7373a7aa9d2727446a8962613bc5194d
satyam <satyamkale271@gmail.com>
2024-07-30 22:27:15 +0530

    layout file template

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,8 +1,14 @@
 const path = require("path");
 function getMessages(req, res) {
-  res.sendFile(
-    path.join(__dirname, "..", "public", "images", "skimountain.jpg")
-  );
+  // res.sendFile(
+  //   path.join(__dirname, "..", "public", "images", "skimountain.jpg")
+  // );
+
+  res.render("messages", {
+    tittle: "Message to my friends",
+    friend: "Elon musk",
+  });
+
   // res.send("<ul><li>hello satyam </li></ul>");
 }
 function postMessage(req, res) {
